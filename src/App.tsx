fa10985f3e02863ab9dec07f2c87e58d0e2f876f
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-01 23:33:13 +0700

    FCP-14 | setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
+import routes from "./routes";
+
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={routes.home.value} element={<HomePage />} />
+      <Route path={routes.myProfile.value} element={<ProfilePage />} />
+    </Routes>
   );
 }
 
