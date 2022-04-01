5d0a915b19ea253e1abd60bf32d62e61cddde243
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-02 02:14:41 +0700

    FCP-14 | setup axios & .env

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import ProfilePage from './pages/ProfilePage'
 import HomePage from './pages/HomePage'
 import routes from './routes'
 import './scss/global.scss'
+import { BE_URL } from './constants/config'
 
 function App(): JSX.Element {
+  console.log({ env: BE_URL })
+
   return (
     <Routes>
       <Route path={routes.home.value} element={<HomePage />} />
