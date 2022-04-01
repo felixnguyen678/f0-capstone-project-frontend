2ef04f8ba1fd3d40ed0b7833b619915601d61552
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-02 01:45:38 +0700

    FCP-14 | setup src structure & unit test

---
+++ src/components/pages/Home/components/Title/index.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h1>Hello</h1>
+}
+
+export default Title
---
+++ src/components/pages/Home/components/Title/utils.test.ts
@@ -0,0 +1,10 @@
+describe('utils.ts', () => {
+  it('test unit test for utils methods', () => {
+    const mockValue = 1
+    const expectedValue = 1
+
+    expect(mockValue).toEqual(expectedValue)
+  })
+})
+
+export default {}
