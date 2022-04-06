51029517d0aa9d40a2db21dd0c2570fc0c6108ce
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-07 00:16:02 +0700

    FCP-22 | implement login page

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '../components/pages/LoginPage/components/LoginForm'
+import styles from '../components/pages/LoginPage/styles.module.scss'
+
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <LoginForm />
+    </div>
+  )
+}
+
+export default LoginPage
