d367bf38a6d918cfd08a4cb38001d36774e70cab
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-07 00:16:37 +0700

    FCP-22 | run Eslint fix

--- src/components/pages/LoginPage/components/LoginForm/index.tsx
+++ src/components/pages/LoginPage/components/LoginForm/index.tsx
@@ -1,37 +1,19 @@
-import { 
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  Button
-} from 'reactstrap'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 import styles from './styles.module.scss'
 
-
 const LoginForm = () => {
-
   return (
     <div className={styles.signInForm}>
-        <h2>Sign In</h2>
-        <Form className={styles.formGroup}>
-          <FormGroup>
-            <Label htmlFor="exampleEmail" >Username</Label>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="example@example.com"
-            />
-          </FormGroup>
-          <FormGroup className={styles.form}>
-            <Label htmlFor="examplePassword" >Password</Label>
-            <Input
-              type="password"
-              name="password"
-              id="examplePassword"
-              placeholder="********"
-            />
-          </FormGroup>
+      <h2>Sign In</h2>
+      <Form className={styles.formGroup}>
+        <FormGroup>
+          <Label htmlFor="exampleEmail">Username</Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="example@example.com" />
+        </FormGroup>
+        <FormGroup className={styles.form}>
+          <Label htmlFor="examplePassword">Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="********" />
+        </FormGroup>
         <Button>Submit</Button>
       </Form>
     </div>
