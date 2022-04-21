b0c44a7f0d70b9458f30bd6fdab69aa3e2c4f320
Hoang Phan Le <phanle11vn@gmail.com>
2022-04-21 12:25:18 +0700

    FCP-38 | mock data for container

--- src/components/pages/ContainerPage/component/ContainerList/containerData.ts
+++ src/components/pages/ContainerPage/component/ContainerList/containerData.ts
@@ -3,48 +3,48 @@ export const containerData = [
     containerName: 'f0 app',
     imageName: 'f0 frontend',
     status: 'Running',
-    port: '3000'
+    port: 'Port: 3000'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 frontend',
     status: 'Running',
-    port: '3000'
+    port: 'Port: 3000'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 frontend',
     status: 'Running',
-    port: '3000'
+    port: 'Port: 3000'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 backend',
     status: 'Paused',
-    port: '3001'
+    port: 'Port: 3001'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 backend',
     status: 'Restarting',
-    port: '3001'
+    port: 'Port: 3001'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 backend',
     status: 'Dead',
-    port: '3001'
+    port: 'Port: 3001'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 backend',
     status: 'Exited',
-    port: '3001'
+    port: 'Port: 3001'
   },
   {
     containerName: 'f0 app',
     imageName: 'f0 backend',
     status: 'Created',
-    port: '3001'
+    port: 'Port: 3001'
   }
 ]
